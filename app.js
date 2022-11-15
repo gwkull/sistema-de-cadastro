f68e274506053429e0f1dbc3524495f1cb621d8f
Rafael De Angelis Karlinski <rafael.dak@gmail.com>
2022-11-15 14:48:48 -0300

    iniciou a numeração de mensagens no console.log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
-console.log("Rafael inseriu uma mensagem no console.log")
+console.log("Rafael inseriu a primeira mensagem no console.log")
+console.log("Rafael fez a segunda inserção de texto no console.log")
